Bug fixes (models dynamics noise, graph builder, train)  normalization, remove sigmoid

scripts/decentralized_controller.py
@@ -33,7 +33,7 @@ Q = np.eye(10)
 R = 0.1 * np.eye(10)
 
 # assume p0 = [1,2,3,4,5] * 10000 and v0 = [0, 0, 0, 0, 0]
-x0 = np.array([1, 0, 2, 0, 3, 0, 4, 0, 5, 0]) * 10000
+x0 = np.array([1, 0, 2, 0, 3, 0, 4, 0, 5, 0])
 
 def platoon_dynamics():
     K, S, E = dlqr(A, B, Q, R) # Solve discrete-time LQR
@@ -41,7 +41,7 @@ def platoon_dynamics():
     def dynamics_part_a(x):
         """ Simulate closed-loop dynamics for part a"""
         D = np.random.normal(loc=0, scale=10000, size=5)
-        w = np.zeros((10, 1))
+        w = np.zeros((10,))
         w[::2] = dt * D / m
         return (A - B @ K) @ x + w # u = -K x 
     
@@ -88,14 +88,16 @@ def platoon_comm_delay():
         v = np.array(v) - 1 # e.g. v = [3,4,5] -> v = [2,3,4]
         r = np.array(r) - 1
         v_transform = []
-        for i in range(len(v)):
+        for v_i in v:
             # e.g. v = [0,2] -> v_transform = [0,1,4,5]
-            v_transform.append(2 * v[i])
-            v_transform.append(2 * v[i] + 1)
+            v_transform.append(2 * v_i)
+            v_transform.append(2 * v_i + 1)
         r_transform = []
-        for i in range(len(r)):
-            r_transform.append(2 * r[i])
-            r_transform.append(2 * r[i] + 1)
+        for r_i in r:
+            r_transform.append(2 * r_i)
+            r_transform.append(2 * r_i + 1)
+
+        print(v, v_transform, r, r_transform)
         
         A_vr = A[np.ix_(v_transform,r_transform)]
         B_vr = B[np.ix_(v_transform,r_transform)]
@@ -176,9 +178,10 @@ def platoon_comm_delay():
     print('Minimum cost J delay:', J_delay)
 
     sim_cost = 0
-    for x, u in zip(xs.T, F.T):
+    for x, u in zip(xs.T, us.T):
         sim_cost += (1 / ts) * (x.T @ Q @ x + u.T @ R @ u)
     print('Simulated cost:', sim_cost)
 
 if __name__ == '__main__':
-    data = platoon_dynamics()
+    # data = platoon_dynamics()
+    platoon_comm_delay()

scripts/graph_builder.py
@@ -198,7 +198,7 @@ class MSDGraphBuilder(GraphBuilder):
         return obj
     
 @register_pytree_node_class
-class DelayStructureGraphBuilder(GraphBuilder):
+class VehiclePlatoonGraphBuilder(GraphBuilder):
     def __init__(
             self, path, seed, alpha, dt, m, noise_std, num_initial_states, senders, receivers
         ):
@@ -233,44 +233,52 @@ class DelayStructureGraphBuilder(GraphBuilder):
                             [zeros, zeros, zeros, zeros, B_ii]])
         
         self.state_dim = len(self.A)
-        self.num_vehicles = self.state_dim // 2
+        self.num_nodes = self.state_dim // 2
 
         self.Q = jnp.eye(self.state_dim)
-        self.R = 0.1 * jnp.eye(self.num_vehicles)
+        self.R = 0.1 * jnp.eye(self.num_nodes)
 
         self.K, self.S, _ = dlqr(self.A, self.B, self.Q, 0.1 * jnp.eye(self.state_dim)) # TODO 
         
         # When x > 0, 1 > 2 > 3 > 4 > 5
         def random_initial_state(_, key):
-            signs = jax.random.choice(key, jnp.array([-1, 1]), shape=(self.state_dim,))
-            # pos_w_noise = sign * 0.5 * (jnp.arange(self.num_vehicles) + jax.random.normal(key, shape=(self.num_vehicles,)))
-            # vel_w_noise = sign * 0.5 * (jnp.zeros(self.num_vehicles) + jax.random.normal(key, shape=(self.num_vehicles,))) 
-            
+            signs = jax.random.choice(key, jnp.array([-1, 1]), shape=(self.num_nodes+1,))
+            signs = jnp.array([signs[0], signs[1], signs[0], signs[2], signs[0], signs[3], signs[0], signs[4], signs[0], signs[5]])            
             state0 = 0.5 * signs * (jnp.array([1, 0, 2, 0, 3, 0, 4, 0, 5, 0]) \
                                   + jax.random.normal(key, shape=(self.state_dim,)))
             
             return _, state0
         
-        key = jax.random.key(self.seed) # update key?
+        key = jax.random.key(self.seed)
         keys = jax.random.split(key, self.num_initial_states)
         _, initial_states = jax.lax.scan(random_initial_state, None, keys)
         
         self.initial_states = jnp.array(initial_states)
 
     def _get_norm_stats(self):
-        """ TODO """
-        pass
+        num_timesteps = 100
+        key = jax.random.key(self.seed)
+        keys = jax.random.split(key, self.num_initial_states)
+        _, controls = jax.vmap(self.get_optimal_state_trajectory, in_axes=(0,0,None))(keys, self.initial_states, num_timesteps)
+
+        norm_stats = ml_collections.ConfigDict()
+        norm_stats.control = ml_collections.ConfigDict({
+            'mean': jnp.mean(controls),
+            'std': jnp.std(controls)
+        })
+
+        self.norm_stats = norm_stats
     
     def dynamics_function(self, state, inputs):
         key, control = inputs
-        D = self.noise_std * jax.random.normal(key,(self.num_vehicles,))
+        D = self.noise_std * jax.random.normal(key,(self.num_nodes,))
         w = self.dt / self.m * jnp.array([0, D[0], 0, D[1], 0, D[2], 0, D[3], 0, D[4]])
         next_state = self.A @ state + self.B @ control + w
         return next_state, (next_state, control)
     
-    def get_state_trajectory(self, key, state_0, controls, timesteps):
+    def get_state_trajectory(self, key, state_0, controls, num_timesteps):
         # optimal control only depends on state
-        keys = jax.random.split(key, timesteps)
+        keys = jax.random.split(key, num_timesteps)
         stateT, outputs = jax.lax.scan(self.dynamics_function, state_0, (keys, controls))
         states = jnp.array(outputs[0])
         # states = jnp.concatenate((state_0.reshape(1,-1), states))
@@ -279,15 +287,15 @@ class DelayStructureGraphBuilder(GraphBuilder):
         return states, controls
     
     def optimal_dynamics_function(self, state, key):
-        D = self.noise_std * jax.random.normal(key, (self.num_vehicles,))
+        D = self.noise_std * jax.random.normal(key, (self.num_nodes,))
         w = self.dt / self.m * jnp.array([0, D[0], 0, D[1], 0, D[2], 0, D[3], 0, D[4]])
         control = -self.K @ state
         next_state = self.A @ state + self.B @ control + w
         return next_state, (next_state, control)
     
-    def get_optimal_state_trajectory(self, key, state_0, timesteps):
+    def get_optimal_state_trajectory(self, key, state_0, num_timesteps):
         # optimal control only depends on state
-        keys = jax.random.split(key, timesteps)
+        keys = jax.random.split(key, num_timesteps)
         state_T, outputs = jax.lax.scan(self.optimal_dynamics_function, state_0, keys) 
         states = jnp.array(outputs[0])
         # states = jnp.concatenate((state_0.reshape(1,-1), states))
@@ -296,12 +304,12 @@ class DelayStructureGraphBuilder(GraphBuilder):
         return states, controls
     
     def _setup_graph_params(self):
-        self.n_node = jnp.floor_divide(jnp.array([len(self.A)]), jnp.array(2))
+        self.n_node = jnp.array([self.num_nodes])
         self.n_edge = self.n_node - 1
     
     def get_graph(self, state: jnp.array) -> jraph.GraphsTuple:
         """ Return a graph with node features being [x(k), u(k)] """
-        nodes = state.reshape((5,-1)) # state should have shape (5, 2)
+        nodes = state.reshape((self.num_nodes,-1))
         edges = None
         globals = None
         graph = jraph.GraphsTuple(nodes=nodes,
@@ -323,13 +331,13 @@ class DelayStructureGraphBuilder(GraphBuilder):
     
     def tree_flatten(self):
         children = () # dynamic
-        aux_data = (self.seed, self._add_undirected_edges, self._add_self_loops, self.A, self.B, self.Q, self.R, self.K, self.S, self.initial_states, self.num_initial_states, self.state_dim, self.num_vehicles) # static
+        aux_data = (self.seed, self._add_undirected_edges, self._add_self_loops, self.A, self.B, self.Q, self.R, self.K, self.S, self.initial_states, self.num_initial_states, self.state_dim, self.num_nodes, self.norm_stats) # static
         return (children, aux_data)
     
     @classmethod
     def tree_unflatten(cls, aux_data, children):
         del children
-        obj = object.__new__(DelayStructureGraphBuilder)
+        obj = object.__new__(VehiclePlatoonGraphBuilder)
         obj.seed                    = aux_data[0]
         obj._add_undirected_edges   = aux_data[1]
         obj._add_self_loops         = aux_data[2]
@@ -342,7 +350,8 @@ class DelayStructureGraphBuilder(GraphBuilder):
         obj.initial_states          = aux_data[9]
         obj.num_initial_states      = aux_data[10]
         obj.state_dim               = aux_data[11]
-        obj.num_vehicles            = aux_data[12]
+        obj.num_nodes               = aux_data[12]
+        obj.norm_states             = aux_data[13]
         obj._setup_graph_params()
         return obj
     

scripts/models.py
@@ -42,12 +42,13 @@ class GraphNetworkSimulator(nn.Module):
         EncodeProcessDecode GN 
     """
     system_params: FrozenConfigDict
+    norm_stats: FrozenConfigDict
 
     num_mp_steps: int = 1
     layer_norm: bool = False
     use_edge_model: bool = False
     shared_params: bool = False
-    vel_history: int = 5
+    vel_history: int = 1
     control_history: int = 1
     noise_std: float = 0.0003
 
@@ -167,7 +168,9 @@ class GraphNetworkSimulator(nn.Module):
             next_edges = None
            
             if self.prediction == 'control':
-                next_control = graph.nodes # TODO: is this normalized?
+                next_control = graph.nodes
+                next_control = self.norm_stats.control.mean + self.norm_stats.control.std * next_control
+
                 def platoon_dynamics_function(state, control):
                     state = state.flatten()
                     # reconstructed_control = np.zeros(10)
@@ -200,7 +203,7 @@ class GraphNetworkSimulator(nn.Module):
                                    [zeros, zeros, zeros, zeros, B_ii]])
                     
                     D = noise_std * jax.random.normal(rng,(5,))
-                    w = dt / m * jnp.array([D[0], 0, D[1], 0, D[2], 0, D[3], 0, D[4], 0])
+                    w = dt / m * jnp.array([0, D[0], 0, D[1], 0, D[2], 0, D[3], 0, D[4]])
                     next_state = A @ state + B @ reconstructed_control + w
 
                     return next_state

scripts/train.py
@@ -16,7 +16,7 @@ from flax.training.early_stopping import EarlyStopping
 
 from time import strftime
 from timeit import default_timer
-from graph_builder import DelayStructureGraphBuilder
+from graph_builder import VehiclePlatoonGraphBuilder
 from scripts.models import *
 from utils.data_utils import *
 from utils.jax_utils import *
@@ -46,18 +46,18 @@ def train(config: ml_collections.ConfigDict):
     # Create optimizer
     tx = optax.adam(**config.optimizer_params)
     # Create training and evaluation data loaders
-    gb = DelayStructureGraphBuilder(None, 
+    gb = VehiclePlatoonGraphBuilder(None, 
                                     training_params.seed,
                                     system_params.alpha, 
                                     system_params.dt,
                                     system_params.m,
                                     system_params.noise_std,
-                                    10000,
+                                    int(5e5),
                                     jnp.array([0,1,2,3]),
                                     jnp.array([1,2,3,4]))  
     
     # TODO: make eval initial states out-of-distribution?
-    eval_gb = DelayStructureGraphBuilder(None, 
+    eval_gb = VehiclePlatoonGraphBuilder(None, 
                                          training_params.seed,
                                          system_params.alpha, 
                                          system_params.dt,
@@ -66,7 +66,10 @@ def train(config: ml_collections.ConfigDict):
                                          20, 
                                          jnp.array([0,1,2,3]),
                                          jnp.array([1,2,3,4]))    
+    
+    # print('control mean', gb.norm_stats.control.mean, 'control std', gb.norm_stats.control.std)
     # Initialize training network
+    net_params.norm_stats = gb.norm_stats
     net = create_net(net_params)
     init_state = gb.initial_states[0]
     init_graph = gb.get_graph(init_state)
@@ -93,27 +96,32 @@ def train(config: ml_collections.ConfigDict):
 
         def loss_fn(params, batch_x0s, batch_graphs):
             if loss_function == 'quadratic_loss':
-                def multi_apply_fn(graph, _):
+                def apply_fn(graph, _):
                     pred_graph = state.apply_fn(params, graph, net_rng, rngs={'dropout': dropout_rng})
-                    state_predictions = pred_graph.nodes[:,:,:2].reshape((1, gb.state_dim))
+                    state_predictions = pred_graph.nodes[:,:,:2].reshape((training_params.batch_size, gb.state_dim))
                     control_predictions = pred_graph.nodes[:,:,-1]
                     loss = jnp.sum(state_predictions @ gb.Q @ state_predictions.T   \
                                  + control_predictions @ gb.R @ control_predictions.T)
                     pred_graph = pred_graph._replace(nodes=pred_graph.nodes[:,:,:-1])
                     return pred_graph, loss
-                final_graph, losses = jax.lax.scan(multi_apply_fn, batch_graphs, None, length=training_params.horizon)
+                final_graph, losses = jax.lax.scan(apply_fn, batch_graphs, None, length=training_params.horizon)
                 loss = jnp.sum(losses)
-            if loss_function == 'supervised':
-                def multi_apply_fn(graph, control_targets):
-                    pred_graph = state.apply_fn(params, graph, net_rng, rngs={'dropout': dropout_rng})
-                    control_predictions = pred_graph.nodes[:,:,-1].reshape((1, gb.state_dim))
+            elif loss_function == 'supervised':
+                def apply_fn(graph, control_targets):
+                    def multi_apply(graph, _):
+                        pred_graph = state.apply_fn(params, graph, net_rng, rngs={'dropout': dropout_rng})
+                        control_predictions = pred_graph.nodes[:,:,-1].reshape((1, gb.state_dim))
+                        pred_graph = pred_graph._replace(nodes=pred_graph.nodes[:,:,:-1])
+                        return pred_graph, control_predictions
+                    pred_graph, control_predictions = jax.lax.scan(multi_apply, graph, None, length=training_params.horizon)
+                    control_predictions = jnp.array(control_predictions).reshape((training_params.horizon, gb.state_dim))
                     loss = optax.l2_loss(control_predictions, control_targets)
-                    pred_graph = pred_graph._replace(nodes=pred_graph.nodes[:,:,:-1])
                     return pred_graph, loss
+            
                 subkeys = jax.random.split(key)
                 get_optimal_state_trajectory = jax.vmap(gb.get_optimal_state_trajectory, in_axes=(0,0,None))
-                state_targets, control_targets = get_optimal_state_trajectory(subkeys, batch_x0s, 1)
-                final_graph, losses = jax.lax.scan(multi_apply_fn, batch_graphs, control_targets)
+                state_targets, control_targets = get_optimal_state_trajectory(subkeys, batch_x0s, training_params.horizon)
+                final_graph, losses = jax.lax.scan(apply_fn, batch_graphs, control_targets)
                 loss = jnp.sum(losses)
             return loss
 
@@ -132,7 +140,7 @@ def train(config: ml_collections.ConfigDict):
 
     def rollout(state: TrainState, x0: jnp.array, key: jax.Array):
         loss_function = training_params.loss_function
-        num_ts = system_params.timesteps
+        num_ts = system_params.timesteps // net_params.num_mp_steps
         tf_idxs = jnp.arange(num_ts)
         ts = tf_idxs * system_params.dt
 
@@ -232,17 +240,17 @@ def train(config: ml_collections.ConfigDict):
                                            plot_dir=os.path.join(plot_dir, f'traj_{i}'),
                                            prefix=f'Epoch {epoch}: eval_traj_{i}')
                 
-                mean_J_loss = rollout_error_sum / eval_gb.num_initial_states
-                writer.write_scalars(epoch, add_prefix_to_keys({'loss': mean_J_loss}, 'eval'))
-                print(f'Epoch {epoch}: rollout mean position loss = {jnp.round(mean_J_loss, 4)}')
+                mean_loss = rollout_error_sum / eval_gb.num_initial_states
+                writer.write_scalars(epoch, add_prefix_to_keys({'loss': mean_loss}, 'eval'))
+                print(f'Epoch {epoch}: rollout mean position loss = {jnp.round(mean_loss, 4)}')
 
-                if mean_J_loss < min_error: 
+                if mean_loss < min_error: 
                     # Save best model
-                    min_error = mean_J_loss
+                    min_error = mean_loss
                     with report_progress.timed('checkpoint'):
                         best_model_ckpt.save(state)
                 if epoch > training_params['min_epochs']: # train at least for 'min_epochs' epochs
-                    early_stop = early_stop.update(mean_J_loss)
+                    early_stop = early_stop.update(mean_loss)
                     if early_stop.should_stop:
                         print(f'Met early stopping criteria, breaking at epoch {epoch}')
                         training_params.num_epochs = epoch - init_epoch
@@ -269,13 +277,178 @@ def train(config: ml_collections.ConfigDict):
     with open(run_params_file, "w") as outfile:
         json.dump(config_js, outfile)
 
+def eval(config: ml_collections.ConfigDict):
+    system_params = config.system_params
+    training_params = config.training_params
+    net_params = config.net_params
+    net_params.system_params = system_params
+
+    def create_net(params):
+        return GraphNetworkSimulator(**params)
+    
+    if training_params.dir == None:
+        training_params.dir = os.path.join(os.curdir, f'results/gnc/{training_params.loss_function}_{strftime("%m%d%H%M")}')
+            
+    log_dir = os.path.join(training_params.dir, 'log')
+    checkpoint_dir = os.path.join(training_params.dir, 'checkpoint')
+    plot_dir = os.path.join(training_params.dir, 'plots')
+    eval_plot_dir = os.path.join(plot_dir, 'eval')
+
+    key = jax.random.key(training_params.seed)
+    key, init_rng, net_rng = jax.random.split(key, 3)
+
+    # Create writer for logs
+    writer = metric_writers.create_default_writer(logdir=log_dir)
+    # Create optimizer
+    tx = optax.adam(**config.optimizer_params)
+    # Create training and evaluation data loaders
+    gb = VehiclePlatoonGraphBuilder(None, 
+                                    training_params.seed,
+                                    system_params.alpha, 
+                                    system_params.dt,
+                                    system_params.m,
+                                    system_params.noise_std,
+                                    int(1e5),
+                                    jnp.array([0,1,2,3]),
+                                    jnp.array([1,2,3,4]))  
+    
+    # TODO: make eval initial states out-of-distribution?
+    eval_gb = VehiclePlatoonGraphBuilder(None, 
+                                         training_params.seed,
+                                         system_params.alpha, 
+                                         system_params.dt,
+                                         system_params.m,
+                                         system_params.noise_std,
+                                         20, 
+                                         jnp.array([0,1,2,3]),
+                                         jnp.array([1,2,3,4]))    
+    
+    # print('control mean', gb.norm_stats.control.mean, 'control std', gb.norm_stats.control.std)
+    # Initialize training network
+    net_params.norm_stats = gb.norm_stats
+    net = create_net(net_params)
+    net.training = False
+    init_state = eval_gb.initial_states[0]
+    init_graph = eval_gb.get_graph(init_state)
+    params = net.init(init_rng, init_graph, net_rng)
+
+    state = TrainState.create(
+        apply_fn=net.apply,
+        params=params,
+        tx=tx,
+    )
+
+    # Load previous checkpoint (if applicable)
+    ckpt = checkpoint.Checkpoint(checkpoint_dir)
+    best_model_ckpt = checkpoint.Checkpoint(os.path.join(checkpoint_dir, 'best_model'))
+    state = best_model_ckpt.restore_or_initialize(state)
+
+    print(f"Number of parameters {num_parameters(params)}")
+
+
+    def rollout(state: TrainState, x0: jnp.array, key: jax.Array):
+        loss_function = training_params.loss_function
+        num_ts = system_params.timesteps // net_params.num_mp_steps
+        tf_idxs = jnp.arange(num_ts)
+        ts = tf_idxs * system_params.dt
+
+        state_targets, control_targets = gb.get_optimal_state_trajectory(key, x0, num_ts)
+        graph0 = gb.get_graph(x0)
+        # graph0 = pytrees_stack([graph0])
+        keys = jax.random.split(key, num_ts)
+
+        def forward_pass(graph, inputs):
+            if loss_function == 'quadratic_loss':
+                key, timestep = inputs
+                graph = state.apply_fn(state.params, graph, key)
+                # compute gnc loss
+                state_predictions = (graph.nodes[:,:2]).reshape((10,-1))
+                control_predictions = (graph.nodes[:,-1]).squeeze()
+                # Graph features (state) is updated in the apply_fn
+                graph = graph._replace(nodes=graph.nodes[:,:-1])
+                loss = state_predictions.T @ gb.Q @ state_predictions \
+                     + control_predictions.T @ gb.R @ control_predictions
+                return graph, (state_predictions, control_predictions, loss)
+            elif loss_function == 'supervised':
+                key, timestep = inputs
+                graph = state.apply_fn(state.params, graph, key)
+                state_predictions = (graph.nodes[:,:2]).reshape((10,-1))
+                control_predictions = (graph.nodes[:,-1]).squeeze()
+                position_predictions = state_predictions[::2].squeeze()
+                position_targets = state_targets.reshape((10,-1))[::2, timestep]
+                graph = graph._replace(nodes=graph.nodes[:,:-1])
+                loss = optax.l2_loss(position_predictions, position_targets)
+                return graph, (state_predictions, control_predictions, loss)
+
+        graphT, pred_data = jax.lax.scan(forward_pass, graph0, (keys, tf_idxs))
+
+        exp_data = (state_targets, control_targets) # Optimal LQR state and control
+        losses = pred_data[2]
+        pred_data = pred_data
+        total_loss = jnp.sum(losses)
+        
+        return ts, pred_data, exp_data, EvalMetrics.single_from_model_output(loss=total_loss)
+
+    # # Create evaluation network
+    # eval_net = create_net(net_params)
+    # eval_net.training = False
+    # eval_state = state.replace(apply_fn=eval_net.apply)
+
+    # Create logger to report training progress
+    report_progress = periodic_actions.ReportProgress(
+        num_train_steps=training_params.num_epochs,
+        writer=writer
+    )
+
+
+    steps_per_epoch = gb.num_initial_states // training_params.batch_size
+    # Setup training epochs
+    init_epoch = int(state.step) // steps_per_epoch + 1
+    final_epoch = init_epoch + training_params.num_epochs
+    training_params.num_epochs = final_epoch
+
+
+    min_error = jnp.inf
+    print("Start training")
+
+    eval_metrics = None
+    net.training = False
+    rollout_error_sum = 0
+    for i in range(len(eval_gb.initial_states)):
+        key, eval_key = jax.random.split(key)
+        ts, pred_data, exp_data, eval_metrics = rollout(state, eval_gb.initial_states[i], eval_key)
+        rollout_error_sum += eval_metrics.compute()['loss']
+        writer.write_scalars(i, add_prefix_to_keys({'loss': eval_metrics.compute()['loss']}, f'eval_{training_params.trial_name}'))
+        plot_evaluation_curves(ts, pred_data, exp_data, None,
+                                plot_dir=eval_plot_dir,
+                                prefix=f'eval_traj_{i}')
+    
+    mean_loss = rollout_error_sum / eval_gb.num_initial_states
+    print(f'Rollout mean position loss = {jnp.round(mean_loss, 4)}')
+
+    # Save config to json
+    eval_metrics = {
+        'mean_rollout_error': mean_loss,
+        'system_params': system_params,
+    }
+
+    config_js = config.to_json_best_effort()
+    run_params_file = os.path.join(training_params.dir, f'eval_{training_params.trial_name}_params.js')
+    with open(run_params_file, "w") as outfile:
+        json.dump(config_js, outfile)
+
 if __name__ == '__main__':
     from argparse import ArgumentParser
     from scripts.config import create_gnc_config
 
     parser = ArgumentParser()
     parser.add_argument('--dir', type=str)
+    parser.add_argument('--eval', action='store_true')
     args = parser.parse_args()
 
     cfg = create_gnc_config(args)
-    train(cfg)
+
+    if args.eval:
+        eval(cfg)
+    else:
+        train(cfg)